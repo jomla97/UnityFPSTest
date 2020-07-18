5f2e19fdfc285b169125fb4ca0732a1106aec272
jomla97 <jomla97@gmail.com>
2020-07-19 00:53:36 +0200

    Pause menu script

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject hud;
+    public GameObject canvas;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ResumeGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resume");
+
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        hud.SetActive(true);
+        canvas.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
