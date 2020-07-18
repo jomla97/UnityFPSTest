c9287a35302203713557accd7b019811a3cc7142
jomla97 <jomla97@gmail.com>
2020-07-18 23:14:23 +0200

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform camera;
+    public CharacterController controller;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float mouseSensitivity = 150f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.5f;
+
+    private float xRotation = 0f;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //Camera movement
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        camera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        //Walking & running
+        float movementSpeed = walkSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = runSpeed;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * movementSpeed * Time.deltaTime);
+
+        //Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerPickupObject.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickupObject : MonoBehaviour{
+    public Transform camera;
+    public Transform objectGrabPosition;
+    public float maxPickupDistance = 3f;
+    public float throwForce = 10f;
+    public float maxObjectMass = 50f;
+
+    private RaycastHit hit;
+    private GameObject grabbedObject;
+    private float originalObjectBounciness;
+
+    void GrabObject()
+    {
+        grabbedObject = hit.transform.gameObject;
+        grabbedObject.GetComponent<Rigidbody>().freezeRotation = true;
+
+        if (grabbedObject.GetComponent<Collider>())
+        {
+            originalObjectBounciness = grabbedObject.GetComponent<Collider>().material.bounciness;
+
+            //Set the object bounciness to 0
+            SetObjectBounciness(0);
+        }
+    }
+
+    void ReleaseObject()
+    {
+        grabbedObject.GetComponent<Rigidbody>().freezeRotation = false;
+
+        //Reset the object bounciness to original value
+        SetObjectBounciness(originalObjectBounciness);
+
+        grabbedObject = null;
+    }
+
+    void ThrowObject()
+    {
+        //Reset the object bounciness to original value
+        SetObjectBounciness(originalObjectBounciness);
+
+        grabbedObject.GetComponent<Rigidbody>().freezeRotation = false;
+        grabbedObject.GetComponent<Rigidbody>().AddForce(camera.transform.forward * (throwForce / grabbedObject.GetComponent<Rigidbody>().mass));
+
+        grabbedObject = null;
+    }
+
+    void SetObjectBounciness(float bounciness)
+    {
+        if (grabbedObject.GetComponent<Collider>())
+        {
+            grabbedObject.GetComponent<Collider>().material.bounciness = bounciness;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Grab object
+        if (Input.GetKeyDown(KeyCode.F) && grabbedObject == null && Physics.Raycast(camera.position, camera.forward, out hit, maxPickupDistance) && hit.transform.GetComponent<Rigidbody>() && hit.transform.GetComponent<Rigidbody>().mass <= maxObjectMass)
+        {
+            Debug.Log("Grabbing object!");
+            GrabObject();
+        }
+        //Release grabbed object
+        else if (Input.GetKeyDown(KeyCode.F) && grabbedObject != null)
+        {
+            Debug.Log("Releasing object");
+            ReleaseObject();
+        }
+        //Throw grabbed object
+        else if (Input.GetMouseButtonDown(0) && grabbedObject != null)
+        {
+            Debug.Log("Throwing object!");
+            ThrowObject();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (grabbedObject != null)
+        {
+            grabbedObject.GetComponent<Rigidbody>().velocity = (objectGrabPosition.position - grabbedObject.transform.GetComponent<Renderer>().bounds.center) * 10;
+        }
+    }
+}
